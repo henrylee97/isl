45fc76e875b324ace513b90e75c44aa16b02fb7c
Henry Lee <gbvrcx@gmail.com>
2021-11-29 07:12:38 +0000

    feat: densenet

---
+++ cisl/model/densenet.py
@@ -0,0 +1,19 @@
+from torch import nn
+
+
+def DenseNet(in_features: int, n_class: int = 10, bias: bool = True) -> nn.Module:
+    model = nn.Sequential(
+        nn.Linear(in_features, 512, bias=bias),
+        nn.ReLU(),
+        nn.Linear(512, 256, bias=bias),
+        nn.ReLU(),
+        nn.Linear(256, 128, bias=bias),
+        nn.ReLU(),
+        nn.Linear(128, 64, bias=bias),
+        nn.ReLU(),
+        nn.Linear(64, 32, bias=bias),
+        nn.ReLU(),
+        nn.Linear(32, n_class, bias=bias),
+        nn.Softmax(),
+    )
+    return model
