3824d7cac170b340e03e6025e92f8bf071786551
Henry Lee <gbvrcx@gmail.com>
2021-11-29 03:33:13 +0000

    feat: mnist dataloader

---
+++ cisl/datasets/dataset.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import Iterable
+
+from cisl.utils.string import StringBuilder
+
+
+class Dataset(ABC):
+
+    @abstractmethod
+    def __init__(self, cache_dir: str = '.cisl') -> None:
+        self._cache_dir = cache_dir
+
+        self._train: Iterable = None
+        self._test: Iterable = None
+
+        self._n_class: int = None
+        self._n_train: int = None
+        self._n_test: int = None
+
+    @property
+    def train(self) -> Iterable:
+        return self._train
+
+    @property
+    def test(self) -> Iterable:
+        return self._test
+
+    def __str__(self):
+        builder = StringBuilder()
+        builder.append_line(f'Dataset {self.__class__.__name__}')
+        builder.indent().append_line(f'Root location: {self._cache_dir}')
+        builder.append_line(f'Number of classes: {self._n_class}')
+        builder.append_line('Train split')
+        builder.indent().append_line(f'Number of datapoints: {self._n_train}')
+        builder.dedent().append_line('Test split')
+        builder.indent().append_line(f'Number of datapoints: {self._n_test}')
+        return str(builder)
