fe4262bea056023cab2511b2a0ad91f6adb1b828
Henry Lee <gbvrcx@gmail.com>
2021-12-03 09:28:35 +0000

    perf: not download dataset for test

--- isl/datasets/cifar.py
+++ isl/datasets/cifar.py
@@ -9,11 +9,12 @@ class CIFAR10(Dataset):
         super(CIFAR10, self).__init__(cache_dir)
 
         self._train = datasets.CIFAR10(self._cache_dir, train=True, download=True)
-        self._test = datasets.CIFAR10(self._cache_dir, train=False, download=True)
+        self._test = datasets.CIFAR10(self._cache_dir, train=False, download=False)
 
         self._n_class = len(self._train.classes)
         self._n_train = len(self._train)
         self._n_test = len(self._test)
+
 
 class CIFAR100(Dataset):
 
@@ -21,7 +22,7 @@ class CIFAR100(Dataset):
         super(CIFAR100, self).__init__(cache_dir)
 
         self._train = datasets.CIFAR100(self._cache_dir, train=True, download=True)
-        self._test = datasets.CIFAR100(self._cache_dir, train=False, download=True)
+        self._test = datasets.CIFAR100(self._cache_dir, train=False, download=False)
 
         self._n_class = len(self._train.classes)
         self._n_train = len(self._train)
--- isl/datasets/mnist.py
+++ isl/datasets/mnist.py
@@ -9,7 +9,7 @@ class MNIST(Dataset):
         super(MNIST, self).__init__(cache_dir)
 
         self._train = datasets.MNIST(self._cache_dir, train=True, download=True)
-        self._test = datasets.MNIST(self._cache_dir, train=False, download=True)
+        self._test = datasets.MNIST(self._cache_dir, train=False, download=False)
 
         self._n_class = len(self._train.classes)
         self._n_train = len(self._train)
