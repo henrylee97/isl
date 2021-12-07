a55dfb8fa45b8615d4fd6697ba9cc0c91b57bd53
Henry Lee <gbvrcx@gmail.com>
2021-12-07 07:17:21 +0000

    feat: parameterized densenet

--- isl/models/densenet.py
+++ isl/models/densenet.py
@@ -1,19 +1,11 @@
 from torch import nn
 
 
-def DenseNet(in_features: int, n_class: int, bias: bool = True) -> nn.Module:
-    model = nn.Sequential(
-        nn.Linear(in_features, 512, bias=bias),
-        nn.ReLU(),
-        nn.Linear(512, 256, bias=bias),
-        nn.ReLU(),
-        nn.Linear(256, 128, bias=bias),
-        nn.ReLU(),
-        nn.Linear(128, 64, bias=bias),
-        nn.ReLU(),
-        nn.Linear(64, 32, bias=bias),
-        nn.ReLU(),
-        nn.Linear(32, n_class, bias=bias),
-        nn.Softmax(),
-    )
-    return model
+def DenseNet(in_features: int, n_class: int, n_hidden: int = 512, n_layers: int = 6, bias: bool = True) -> nn.Module:
+    layers = [nn.Linear(in_features, n_hidden, bias=bias), nn.ReLU()]
+    for _ in range(n_layers - 2):
+        layers.append(nn.Linear(n_hidden, n_hidden, bias=bias))
+        layers.append(nn.ReLU())
+    layers.append(nn.Linear(n_hidden, n_class, bias=bias))
+    layers.append(nn.Softmax())
+    return nn.Sequential(*layers)
