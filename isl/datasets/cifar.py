3cc18c43edee034256817e18d6dd3771eed43630
Henry Lee <gbvrcx@gmail.com>
2021-12-07 07:09:02 +0000

    fix: make tensor and normalize

--- isl/datasets/cifar.py
+++ isl/datasets/cifar.py
@@ -1,4 +1,7 @@
 from torchvision import datasets
+from torchvision.transforms import Compose
+from torchvision.transforms import ToTensor
+from torchvision.transforms import Normalize
 
 from isl.datasets.dataset import Dataset
 
@@ -8,8 +11,9 @@ class CIFAR10(Dataset):
     def __init__(self, *args, **kwargs) -> None:
         super(CIFAR10, self).__init__(*args, **kwargs)
 
-        self._train = datasets.CIFAR10(self._cache_dir, train=True, download=self._download)
-        self._test = datasets.CIFAR10(self._cache_dir, train=False, download=False)
+        transform = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        self._train = datasets.CIFAR10(self._cache_dir, train=True, transform=transform, download=self._download)
+        self._test = datasets.CIFAR10(self._cache_dir, train=False, transform=transform, download=False)
 
         self._n_class = len(self._train.classes)
         self._n_train = len(self._train)
@@ -21,8 +25,9 @@ class CIFAR100(Dataset):
     def __init__(self, *args, **kwargs) -> None:
         super(CIFAR100, self).__init__(*args, **kwargs)
 
-        self._train = datasets.CIFAR100(self._cache_dir, train=True, download=self._download)
-        self._test = datasets.CIFAR100(self._cache_dir, train=False, download=False)
+        transform = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        self._train = datasets.CIFAR100(self._cache_dir, train=True, transform=transform, download=self._download)
+        self._test = datasets.CIFAR100(self._cache_dir, train=False, transform=transform, download=False)
 
         self._n_class = len(self._train.classes)
         self._n_train = len(self._train)
--- isl/datasets/mnist.py
+++ isl/datasets/mnist.py
@@ -1,4 +1,7 @@
 from torchvision import datasets
+from torchvision.transforms import Compose
+from torchvision.transforms import ToTensor
+from torchvision.transforms import Normalize
 
 from isl.datasets.dataset import Dataset
 
@@ -8,8 +11,9 @@ class MNIST(Dataset):
     def __init__(self, *args, **kwargs):
         super(MNIST, self).__init__(*args, **kwargs)
 
-        self._train = datasets.MNIST(self._cache_dir, train=True, download=self._download)
-        self._test = datasets.MNIST(self._cache_dir, train=False, download=False)
+        transform = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        self._train = datasets.MNIST(self._cache_dir, train=True, transform=transform, download=self._download)
+        self._test = datasets.MNIST(self._cache_dir, train=False, transform=transform, download=False)
 
         self._n_class = len(self._train.classes)
         self._n_train = len(self._train)
