35aaaa18eb28318ae08a3bb8293fff3f3d225d9c
Henry Lee <gbvrcx@gmail.com>
2021-12-08 02:47:37 +0000

    fix: mnist has one channel

--- isl/datasets/mnist.py
+++ isl/datasets/mnist.py
@@ -11,9 +11,11 @@ class MNIST(Dataset):
     def __init__(self, *args, **kwargs):
         super(MNIST, self).__init__(*args, **kwargs)
 
-        transform = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        self._train = datasets.MNIST(self._cache_dir, train=True, transform=transform, download=self._download)
-        self._test = datasets.MNIST(self._cache_dir, train=False, transform=transform, download=False)
+        transform = Compose([ToTensor(), Normalize(0.5, 0.5)])
+        self._train = datasets.MNIST(
+            self._cache_dir, train=True, transform=transform, download=self._download)
+        self._test = datasets.MNIST(
+            self._cache_dir, train=False, transform=transform, download=False)
 
         self._n_class = len(self._train.classes)
         self._n_train = len(self._train)
